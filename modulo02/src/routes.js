d6101d8794ace35cd647d36ac3e40ac7c59ebb9f
yonlu <lss.emailbox@gmail.com>
2019-07-01 16:22:11 -0400

    feature: added listing for providers and customers

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -3,6 +3,10 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -15,8 +19,13 @@ routes.use(authMiddleware);
 
 routes.put('/users', authMiddleware, UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
