57eead961559dd16b157d5ff5bcea90da475b104
yonlu <lss.emailbox@gmail.com>
2019-07-03 12:23:13 -0400

    feature: added automatic emails on appointment cancel

---
+++ modulo02/src/app/jobs/CancellationMail.js
@@ -0,0 +1,27 @@
+import { format, parseISO } from 'date-fns';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    console.log('Queue executed');
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Appointment canceled',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(parseISO(appointment.date), "MMMM dd' at' H:mm"),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '97f1ea8121e4db',
+    pass: '661f1824a639f3',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
