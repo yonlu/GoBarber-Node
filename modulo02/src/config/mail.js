f64450ec72d565482729c01732524124d106f362
yonlu <lss.emailbox@gmail.com>
2019-07-03 15:14:31 -0400

    chore: added ENV variables and virtual fields to appointments

--- modulo02/src/config/mail.js
+++ modulo02/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '97f1ea8121e4db',
-    pass: '661f1824a639f3',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'GoBarber Team <noreply@gobarber.com>',
---
+++ modulo02/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://f7e4d5b542664f03b947939744d1bba4@sentry.io/1496456',
+};
