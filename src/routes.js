c690bec4844a7da3c63eb377e0a55eee2bcb1301
yonlu <lss.emailbox@gmail.com>
2019-08-30 15:42:28 -0400

    chore: removed testing route

--- src/routes.js
+++ src/routes.js
@@ -17,8 +17,6 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.get('/', (req, res) => res.send('ok'));
-
 routes.use(authMiddleware);
 
 routes.put('/users', authMiddleware, UserController.update);
